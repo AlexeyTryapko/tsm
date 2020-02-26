3d047e12898e09e7ae7f0b7f9446efcc49798640
Alexey Tryapko <aatryapko1201@gmail.com>
2020-02-27 00:21:17 +0200

    connect containers to root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './style.css';
+import { chart, nodes } from './configs/mock';
+import Workspace from './containers/Workspace';
+import NodesSidebar from './containers/NodesSidebar';
 
-const App = () => (
-    <div>
-        <h1>Hello world</h1>
-    </div>
-);
+const App = () => {
+    return (
+        <div className="page-content">
+            <NodesSidebar nodes={nodes} />
+            <Workspace chart={chart} />
+        </div>
+    );
+};
 
 export default App;
