36e74277a2f0f040dede578c875a21d571a7070d
Alexey Tryapko <aatryapko1201@gmail.com>
2020-02-27 00:20:44 +0200

    create scafold of properties sidebar

---
+++ src/containers/PropertiesSidebar/index.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const PropertiesSidebar = () => (
+    <div className="sidebar">
+        <div className="sidebar-title">
+            Drag and drop these items onto the canvas.
+        </div>
+    </div>
+);
+
+export default PropertiesSidebar;
