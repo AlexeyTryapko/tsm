903054ff81f517864bd86e37b39c2d58e45daf67
Alexey <alexey.tryapko@binary-studio.com>
2020-03-03 08:11:46 +0200

    update code style and export model fcns

--- src/services/functions.js
+++ src/services/functions.js
@@ -1,86 +1,90 @@
-const {PI, sin, floor, random} = Math
+const { PI, sin, floor, random } = Math;
 
 function signalSource(simulationParams, blockParams, step) {
     let A = blockParams['amplitude'];
     let f = blockParams['frequency'];
     let message = blockParams['sequence'];
     let T = simulationParams['periodOfSignalUnit'];
-    let dt = simulationParams['quantizationPeriod']
-    let current = floor(step*dt/T);
+    let dt = simulationParams['quantizationPeriod'];
+    let current = floor((step * dt) / T);
 
-    if(current <= message.lenght-1) {
-        if(blockParams['signalType'] == 'manchesterСode'){
-            if(message[current] == '1'){
-                if((step*dt/T-current) <= 0.5) {
+    if (current <= message.lenght - 1) {
+        if (blockParams['signalType'] == 'manchesterСode') {
+            if (message[current] == '1') {
+                if ((step * dt) / T - current <= 0.5) {
                     return A;
                 } else {
                     return -A;
                 }
             } else {
-                if((step*dt/T-current) <= 0.5) {
+                if ((step * dt) / T - current <= 0.5) {
                     return -A;
                 } else {
                     return A;
                 }
             }
         } else {
-            if(message[current] == '1') {
-                return A*sin(2*PI*f*step*dt);
+            if (message[current] == '1') {
+                return A * sin(2 * PI * f * step * dt);
             } else {
-                return A*sin(2*PI*f*step*dt + PI);
+                return A * sin(2 * PI * f * step * dt + PI);
             }
         }
     }
     return 0;
 }
 
-
 function referenceSource(simulationParams, blockParams, step) {
     let A = blockParams['amplitude'];
     let f = blockParams['frequency'];
     let outofsync = blockParams['outofsync'];
     let T = simulationParams['periodOfSignalUnit'];
-    let dt = simulationParams['quantizationPeriod']
-    let current = floor(step*dt/T);
+    let dt = simulationParams['quantizationPeriod'];
+    let current = floor((step * dt) / T);
 
-    if(blockParams['signalType'] == 'manchesterСode'){
-        if((step*dt/T-current+outofsync/100) <= 0.5) {
+    if (blockParams['signalType'] == 'manchesterСode') {
+        if ((step * dt) / T - current + outofsync / 100 <= 0.5) {
             return A;
         } else {
             return -A;
         }
     } else {
-        return A*sin(2*PI*f*(step*dt+T*outofsync/100));
+        return A * sin(2 * PI * f * (step * dt + (T * outofsync) / 100));
     }
 }
 
-
-function noise(simulationParams, blockParams, step){
+function noise(simulationParams, blockParams, step) {
     let noiseType = blockParams['noiseType'];
     let A = blockParams['amplitude'];
     let rand = 0;
 
-    if(noiseType == 'whiteNoise'){
-        rand = 2*(random()-0.5);
+    if (noiseType == 'whiteNoise') {
+        rand = 2 * (random() - 0.5);
     }
-    if(noiseType == 'pinkNoise'){
-        rand = 2*(random()-0.5);
+    if (noiseType == 'pinkNoise') {
+        rand = 2 * (random() - 0.5);
     }
-    if(noiseType == 'blueNoise'){
-        rand = 2*(random()-0.5);
+    if (noiseType == 'blueNoise') {
+        rand = 2 * (random() - 0.5);
     }
-    if(noiseType == 'grayNoise'){
-        rand = 2*(random()-0.5);
+    if (noiseType == 'grayNoise') {
+        rand = 2 * (random() - 0.5);
     }
-    if(noiseType == 'pinkNoise'){
-        rand = 2*(random()-0.5);
+    if (noiseType == 'pinkNoise') {
+        rand = 2 * (random() - 0.5);
     }
-    if(noiseType == 'impulseNoise'){
-        rand = 2*(random()-0.5);
+    if (noiseType == 'impulseNoise') {
+        rand = 2 * (random() - 0.5);
     }
-    if(noiseType == 'randomNoise'){
-        rand = 2*(random()-0.5);
+    if (noiseType == 'randomNoise') {
+        rand = 2 * (random() - 0.5);
     }
 
-    return A*rand;
+    return A * rand;
 }
+
+export default {
+    signalSource,
+    referenceSource,
+    noise,
+};
--- src/services/simulation.js
+++ src/services/simulation.js
@@ -1,66 +1,66 @@
-
+import { signalSource, referenceSource, noise } from './functions';
 
 export const start = chart => {
-    console.log('Start');
-    let func_names = {					
-    	'SIGNAL SOURCE':'signalSource', //TODO use func instead of strings
-    }
+    let func_names = {
+        'SIGNAL SOURCE': signalSource,
+        'REFERENCE SOURCE': referenceSource,
+        NOISE: noise,
+    };
 
     let obj_idxs = {};
     let obj_id = 0;
 
-    for(let node_id in chart["nodes"]){
-    	let node = chart["nodes"][node_id];
-    	if(!(node.id in obj_idxs)){
-    		obj_idxs[node.id]=obj_id;
-    		obj_id=obj_id+1;
-    	}
-	}
+    for (let node_id in chart['nodes']) {
+        let node = chart['nodes'][node_id];
+        if (!(node.id in obj_idxs)) {
+            obj_idxs[node.id] = obj_id;
+            obj_id = obj_id + 1;
+        }
+    }
 
-	let matrix = new Array(obj_id).fill(new Array(obj_id).fill(0));
+    let matrix = new Array(obj_id).fill(new Array(obj_id).fill(0));
     let inputs = new Array(obj_id).fill(0);
     let outputs = new Array(obj_id).fill(0);
     let Y = new Array(obj_id).fill(undefined);
 
-	for(let link_id in chart["links"]){
-		let link = chart["links"][link_id];
-		let to_idx = obj_idxs[link.to.nodeId];
-		let from_idx = obj_idxs[link.from.nodeId];
-		inputs[to_idx]+=1;
-		outputs[from_idx]+=1;
-		matrix[from_idx][to_idx]+=1;
-	}
+    for (let link_id in chart['links']) {
+        let link = chart['links'][link_id];
+        let to_idx = obj_idxs[link.to.nodeId];
+        let from_idx = obj_idxs[link.from.nodeId];
+        inputs[to_idx] += 1;
+        outputs[from_idx] += 1;
+        matrix[from_idx][to_idx] += 1;
+    }
 
-	for(let [idx, value] of outputs.entries()){
-		if(value == 0){
-			Y[idx] = 'end-point';
-		}
-	}
+    for (let [idx, value] of outputs.entries()) {
+        if (value == 0) {
+            Y[idx] = 'end-point';
+        }
+    }
 
-	function calc(idx){
-		let x = [];
-		for(let [row_idx, row] of matrix.entries()){
-			if(row[idx]!==0){
-				if(Y[row_idx]===undefined){
-					Y[row_idx]=calc(row_idx);
-				}
-				x.push(Y[row_idx]);
-			}
-		}
-		let node_id = undefined
-		for(let [_node_id,node_idx] of obj_idxs.entries()){
-			if(node_idx===idx){
-				node_id = _node_id;
-				break;
-			}
-		}
-		return func_names[chart["nodes"][node_id]](x,params);//TODO Correct call
-	}
+    function calc(idx) {
+        let x = [];
+        for (let [row_idx, row] of matrix.entries()) {
+            if (row[idx] !== 0) {
+                if (Y[row_idx] === undefined) {
+                    Y[row_idx] = calc(row_idx);
+                }
+                x.push(Y[row_idx]);
+            }
+        }
+        let node_id = undefined;
+        for (let [_node_id, node_idx] of obj_idxs.entries()) {
+            if (node_idx === idx) {
+                node_id = _node_id;
+                break;
+            }
+        }
+        return func_names[chart['nodes'][node_id]](x, params); //TODO Correct call
+    }
 
-	for(let [idx, value] of Y.entries()){
-		if(value==='end-point'){
-			
-			Y[idx]=calc(idx);
-		}
-
+    for (let [idx, value] of Y.entries()) {
+        if (value === 'end-point') {
+            Y[idx] = calc(idx);
+        }
+    }
 };
